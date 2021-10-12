8053061f74db0c58a18ac81e429f2473a00a2d8e
vitto2 <vitor2071@gmail.com>
2021-10-12 19:39:53 -0300

    Implementando compra com cartao de credito

--- meu_banco.rb
+++ meu_banco.rb
@@ -4,7 +4,8 @@ class Banco
  
      def initialize(deposito)
          @deposito = deposito; 
-         @sacar = 0;  
+         @sacar = 0;
+         @limite_cred = 0;   
  
          if deposito <= 2000
              @limite_cred = (deposito / 100) * 25;  
@@ -19,7 +20,7 @@ class Banco
          @deposito =  @deposito + deposito;
          @saldo = @deposito; 
 
-         puts "Deposito feito com sucesso. Seu novo .,ç;saldo é de #{@saldo}"
+         puts "Deposito feito com sucesso. Seu novo saldo é de #{@saldo}"
      end
  
      def saldo
@@ -31,17 +32,25 @@ class Banco
          puts "Saque efetuado com sucesso."
      end 
  
-     def limite 
-         puts "Limite disponível #{@limite_cred}"
+     def limite_credito
+         puts "Limite disponível R$#{@limite_cred}"
+     end 
+
+     def compra_credito(valor_compra)
+       @limite_cred = @limite_cred - valor_compra
+        puts "Compra no valor de R$#{valor_compra} aprovada."
      end 
  end 
  
 cliente_1 = Banco.new(10000)
+cliente_1.limite_credito
 cliente_1.depositar(5000)
 cliente_1.depositar(2000)
 puts "Seu saldo é #{cliente_1.saldo}"
 cliente_1.sacar(1000)
-puts "Seu novo saldo é #{cliente_1.saldo}" 
- 
-
-  
+puts "Seu novo saldo é #{cliente_1.saldo}"
+cliente_1.depositar(10000)
+cliente_1.sacar(3000)
+puts "Seu novo saldo é #{cliente_1.saldo}"
+cliente_1.compra_credito(1559.90)
+cliente_1.limite_credito
