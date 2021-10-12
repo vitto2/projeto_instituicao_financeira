4bf455871a520ed68bf433282592c4db23d32bfc
vitto2 <vitor2071@gmail.com>
2021-10-12 19:17:02 -0300

    Subindo funcionalidade de saque

--- meu_banco.rb
+++ meu_banco.rb
@@ -18,25 +18,30 @@ class Banco
      def depositar(deposito) 
          @deposito =  @deposito + deposito;
          @saldo = @deposito; 
+
+         puts "Deposito feito com sucesso. Seu novo .,ç;saldo é de #{@saldo}"
      end
  
      def saldo
-         puts  @saldo
+         @saldo
      end  
  
      def sacar(valor_saque) 
-          
+        @saldo = @saldo - valor_saque; 
          puts "Saque efetuado com sucesso."
      end 
- 
-     
  
      def limite 
          puts "Limite disponível #{@limite_cred}"
      end 
  end 
  
- cliente_1 = Banco.new(10000)
- cliente_1.depositar(5000)
- cliente_1.saldo 
+cliente_1 = Banco.new(10000)
+cliente_1.depositar(5000)
+cliente_1.depositar(2000)
+puts "Seu saldo é #{cliente_1.saldo}"
+cliente_1.sacar(1000)
+puts "Seu novo saldo é #{cliente_1.saldo}" 
+ 
+
   
